1ef5a76f4e5645f43f4fc7ce0939ea7884d05c84
GodiSouleymane <ssabiougodi@gmail.com>
2024-01-24 17:52:01 +0000

    fonction d'ajout des images dans la sous collection

--- lib/services/dbService.dart
+++ lib/services/dbService.dart
@@ -52,4 +52,39 @@ class DataBaseService {
       }).toList();
     });
   }
+
+  // ajout de la voiture faavorit dans la sous colleection;
+
+  void addFavoritePicture(Picture picture, String userID) async {
+    final picDocRef = _pics.doc(picture.picID);
+    final favoritedBy = picDocRef.collection('favoritedBy');
+    int picFavoriteCount = picture.picFavoriteCount!;
+    int increaseCount = picFavoriteCount += 1;
+    favoritedBy.doc(userID).set({
+       "picDesc": picture.picDesc,
+        "picUrlImg": picture.picUrlImg,
+        "picUserID": picture.picUserID,
+        "picUserName": picture.picUserName,
+        "picTimeStamp": picture.picTimeStamp,
+        "picFavoriteCount": increaseCount,
+    });
+
+    picDocRef.update({
+       "picFavoriteCount": increaseCount,
+    });
+  }
+
+  // Retirer la photo de la liste des favoris;
+
+  void removeFavoritePicture(Picture picture, String userID) {
+    final picDocRef = _pics.doc(picture.picID);
+    final favoritedBy = picDocRef.collection('favoritedBy');
+    int picFavoriteCount = picture.picFavoriteCount!;
+    int decreaseCount = picFavoriteCount -= 1;
+
+     picDocRef.update({
+       "picFavoriteCount": decreaseCount,
+    });
+    favoritedBy.doc(userID).delete();
+  }
 }
