5d73c015578119df646a0c7e11754656db35ade4
GodiSouleymane <ssabiougodi@gmail.com>
2024-01-24 17:29:48 +0000

    ajout de l'icone de like

--- lib/shared-ui/favoriteBadge.dart
+++ lib/shared-ui/favoriteBadge.dart
@@ -11,11 +11,43 @@ class FavoriteBadge extends StatefulWidget {
 }
 
 class _FavoriteBadgeState extends State<FavoriteBadge> {
+  bool isMyFavoritePicture =  false;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white.withOpacity(0.7)
+    return Positioned(
+      top: 4.0,
+      right: 12.0,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 4.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          color: Colors.white.withOpacity(0.3)
+        ),
+        child: isMyFavoritePicture ? Row(
+          children: [
+            Text('${widget.picture!.picFavoriteCount}',
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.red,
+            ),
+            ),
+            const Icon(
+              Icons.favorite, color: Colors.red,
+            )
+          ],
+        ) : Row(
+          children: [
+            widget.picture!.picFavoriteCount! > 0 ?
+            Text('${widget.picture!.picFavoriteCount}',
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+            ) : Container(),
+            const Icon(
+              Icons.favorite,
+            )
+          ],
+        ),
       ),
     );
   }
